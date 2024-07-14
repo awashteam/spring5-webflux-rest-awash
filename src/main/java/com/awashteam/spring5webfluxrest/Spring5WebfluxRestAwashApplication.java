8f9df7ee26bd93ffcb14a45d20f82064fa9966ce
awash <Emerald190!!>
2024-07-14 15:16:45 -0500

    first commit

---
+++ src/main/java/com/awashteam/spring5webfluxrest/Spring5WebfluxRestAwashApplication.java
@@ -0,0 +1,13 @@
+package com.awashteam.spring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestAwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebfluxRestAwashApplication.class, args);
+	}
+
+}
